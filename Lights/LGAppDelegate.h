02e341a13d7100d352dc3f65c3156089f3facca4
Nathan Borror <nathan@nathanborror.com>
2013-09-07 14:44:06 -0700

    Initial Commit

---
+++ Lights/LGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LGAppDelegate.h
+//  Lights
+//
+//  Created by Nathan Borror on 9/7/13.
+//  Copyright (c) 2013 Nathan Borror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lights/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lights
+//
+//  Created by Nathan Borror on 9/7/13.
+//  Copyright (c) 2013 Nathan Borror. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LGAppDelegate class]));
+  }
+}
